0310e7061e636987b0b0bb844347e611a01964f2
CS106A <>
2019-02-26 22:54:45 -0800

    {"types":"C","runs":62}

--- DarkRoomAlgorithms.java
+++ DarkRoomAlgorithms.java
@@ -123,8 +123,8 @@ public class DarkRoomAlgorithms implements DarkRoomAlgorithmsInterface {
 			for (int c = 0; c < columns; c++) {
 				for (int i = r - 1; i<= r + 1; i++) {
 					for (int j = c - 1; j <= c + 1; j++) {
-						int x = columns + j;
-						int y = rows + i;
+						int x = c + j;
+						int y = r + i;
 						int red = GImage.getRed(pixels[y][x]);
 						redBlur+=red; 
 						int green = GImage.getGreen(pixels[y][x]);
