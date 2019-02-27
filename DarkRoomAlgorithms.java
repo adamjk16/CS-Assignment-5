e852aad88642de4240694b52b2a59b08ebaf27ba
CS106A <>
2019-02-26 23:25:45 -0800

    {"types":"C","runs":86}

--- DarkRoomAlgorithms.java
+++ DarkRoomAlgorithms.java
@@ -122,6 +122,7 @@ public class DarkRoomAlgorithms implements DarkRoomAlgorithmsInterface {
 				int avgPixels = 0;
 				for (int i = r - 1; i<= r + 1; i++) {
 					for (int j = c - 1; j <= c + 1; j++) {
+					if (i >= 0 && i < pixels.length && j >=0 && j < pixels[r].length) {	
 						int red = GImage.getRed(pixels[i][j]);
 						redTotal += red;
 						int green = GImage.getGreen(pixels[i][j]);
@@ -131,6 +132,7 @@ public class DarkRoomAlgorithms implements DarkRoomAlgorithmsInterface {
 						avgPixels++;
 					}
 				}
+				}		
 			int avgRed = redTotal / avgPixels;
 			int avgGreen = greenTotal / avgPixels;
 			int avgBlue = blueTotal / avgPixels;
