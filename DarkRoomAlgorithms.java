09d88da5f428bcdbfd9f8a2a30660c6c0565431e
CS106A <>
2019-02-27 21:27:50 -0800

    {"types":"C","runs":96}

--- DarkRoomAlgorithms.java
+++ DarkRoomAlgorithms.java
@@ -150,8 +150,7 @@ public class DarkRoomAlgorithms implements DarkRoomAlgorithmsInterface {
 
 	public GImage crop(GImage source, int cropX, int cropY, int cropWidth, int cropHeight) {
 		int [][] pixels = source.getPixelArray();
-		c
-		int cropX.getX();
+		
 		
 		
 	}
